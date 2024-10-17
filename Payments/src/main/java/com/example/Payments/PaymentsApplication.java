1c1ec0fe1c1320c65a8462f306d33b269549ea3a
SteshM <mbithestella0@gmail.com>
2024-10-17 10:10:13 +0300

    Added configurations

--- Payments/src/main/java/com/example/Payments/PaymentsApplication.java
+++ Payments/src/main/java/com/example/Payments/PaymentsApplication.java
@@ -2,6 +2,12 @@ package com.example.Payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PaymentsApplication {
@@ -9,5 +15,21 @@ public class PaymentsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentsApplication.class, args);
 	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService(){
+		return new UserDetailsService() {
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				return null;
+			}
+		};
+	}
+
 
 }
